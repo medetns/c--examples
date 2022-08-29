1f11dde1e8a4006c2cce173de394b2baf599cb30
Medet <medetnurm@gmail.com>
2022-08-29 15:50:41 +0600

    first number is the power of secon number

--- examples001/Program.cs
+++ examples001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int firstNumb = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumb = int.Parse(Console.ReadLine());
+if (firstNumb == secondNumb * secondNumb) {
+    Console.WriteLine($"{firstNumb} равен квадрату {secondNumb}");
+}
+else if (secondNumb == firstNumb * firstNumb) {
+    Console.WriteLine($"{secondNumb} равен квадрату {firstNumb}");
+}
+else {
+    Console.WriteLine("NO");
+}
