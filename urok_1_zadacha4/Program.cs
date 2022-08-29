4807975cb852155f99a0bbb7f495a91ed78d1bb0
Medet <medetnurm@gmail.com>
2022-08-29 16:31:32 +0600

    задача 4 1-го семинара

---
+++ urok_1_zadacha4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+if (a >= b && a >= c) {
+    Console.WriteLine(a);
+}
+else if (b >= a && b >= c) {
+    Console.WriteLine(b);
+}
+else {
+    Console.WriteLine(c);
+}
