ecb1fa064d84c0dc620a1ea319ca49addbcc5f82
Medet <medetnurm@gmail.com>
2022-08-29 16:26:28 +0600

    задача 2 1-го семинара

---
+++ urok_1_zadacha2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+if (a > b) {
+    Console.WriteLine($"max = {a}");
+}
+else {
+    Console.WriteLine($"max = {b}");
+}
+
